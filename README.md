faafb149a35a2c8db6c57824bdbdab9bf3545be9
Dirk Bosman <dirkjobosman@gmail.com>
2020-09-01 17:42:56 +0200

    doc updates

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ A (Destructured) Express Node PostgreSQL App w. Controllers
 - [Part 1: node-postgresql-unstructured](https://github.com/dirkbosman/node-postgresql-unstructured)
 - [Part 2: node-postgresql-destructured](https://github.com/dirkbosman/node-postgresql-destructured)
 - [Part 3: node-postgresql-controllers](https://github.com/dirkbosman/node-postgresql-controllers)
+- [Part 4: node-mongodb-controllers](https://github.com/dirkbosman/node-mongodb-controllers)
 
 #### To Do's:
 
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 
-const users = require('./api/users');
-const orders = require('./api/orders');
-const errorHandler = require('./middleware/error');
+const users = require("./api/users");
+const orders = require("./api/orders");
+const errorHandler = require("./middleware/error");
 const app = express();
 
 app.use(express.json());
-app.use('/users', users);
-app.use('/orders', orders);
+app.use("/users", users);
+app.use("/orders", orders);
 app.use(errorHandler);
 
-app.listen('3000', () => console.log('connected'));
-
+app.listen("3000", () => console.log("connected"));
